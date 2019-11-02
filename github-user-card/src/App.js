08b6181d54796d30f60ebdf1bc18656e34a271e3
FreedomWriter <natalienetworks@gmail.com>
2019-11-01 20:37:48 -0500

    App.js, UserCard skeletons, user fetch called

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import UserCard from "./components/userComponents/UserCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    gitUser: [],
+    gitFollowers: []
+  };
+
+  componentDidMount() {
+    fetch("https://api.github.com/users/FreedomWriter")
+      .then(res => res.json())
+      .then(res => {
+        console.log(
+          `nd: index.js: App: componentDidMount: fetch: then: res: `,
+          res
+        );
+      })
+      .catch(err =>
+        console.log(
+          `nd: index.js: App: componentDidMount: fetch: then: err: `,
+          err
+        )
+      );
+  }
+
+  render() {
+    return (
+      <div>
+        <UserCard />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ github-user-card/src/components/userComponents/UserCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function UserCard() {
+  return (
+    <div>
+      <p>Hello from UserCard</p>
+    </div>
+  );
+}
